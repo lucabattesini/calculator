dafc42b917cd7f75c8b7e7f2cd946029ac804125
Luca Battesini <lucabattesini1@gmail.com>
2024-02-07 11:05:40 -0300

    Deleting the functions from scripts

--- scripts.js
+++ scripts.js
@@ -59,19 +59,3 @@ ponto.addEventListener("click", () => {
     screen.innerText = screenNumbers + "."
     screenNumbers = screenNumbers + "."
 })
-
-function back() {
-    const bck = window.document.getElementById("back")
-    bck.addEventListener("click", () => {
-        screenNumbers = screenNumbers.slice(0, -1);
-        screen.innerText = screenNumbers
-    })
-}
-
-function ac() {
-    const ac = window.document.getElementById("ac")
-    ac.addEventListener("click", () => {
-        screenNumbers = ""
-        screen.innerText = ""
-    })
-}
